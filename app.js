150eb3d1eb699332fb065fdf91d15006465f764d
crazykohei0123 <kohei.hayashi@live.com>
2024-01-09 21:51:41 +0900

    Update app.js
    socket mode

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const { App } = require('@slack/bolt');
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN,
+  // ソケットモードではポートをリッスンしませんが、アプリを OAuth フローに対応させる場合、
+  // 何らかのポートをリッスンする必要があります
+  port: process.env.PORT || 3000
+});
   customRoutes: [
     {
       path: '/health-check',
@@ -23,7 +29,7 @@ app.message('hello', async ({ message, say }) => {
 
 (async () => {
   // アプリを起動します
-  await app.start(process.env.PORT || 3000);
+  await app.start();
 
   console.log('⚡️ Bolt app is running!');
 })();
